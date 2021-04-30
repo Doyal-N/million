9f9ab5ab02da01057f11213b3bb881e1d4579790
Doyal-N <coin20@protonmail.com>
2021-04-30 16:20:11 +0500

    style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-success'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def bootstrap_flash_helper(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: 'alert') do
+        concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
+  def image_url_helper
+    asset_pack_path('media/images/bg.jpg')
+  end
 end
