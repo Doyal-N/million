d73472337cf58cd3aa3f322da1e31c2afdcfaea0
Doyal-N <coin20@protonmail.com>
2021-05-04 15:16:06 +0500

    games#show

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
 
   let(:user) { create(:user) }
-  before { login(user) }
+  let(:game) { create(:game_with_questions, user: user) }
 
   describe 'GET #show' do
-    let(:game) { create(:game_with_questions, user: user) }
     let(:current_question) { game.current_game_question }
 
-    before { get :show, params: { id: user.games.find(game.id) } }
+    before do
+      login(user)
+      get :show, params: { id: user.games.find(game.id) }
+    end
 
     it 'assigns game, game_question' do
       expect(assigns(:game)).to eq(game)
@@ -18,6 +20,18 @@ RSpec.describe GamesController, type: :controller do
 
     it 'render show' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #show stranger game' do
+    let(:user2) { create(:user) }
+
+    it 'user do not show stranger game' do
+      login(:user2)
+      get :show, params: { id: game.id }
+
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to(user_session_path)
     end
   end
 end
