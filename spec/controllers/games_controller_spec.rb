16b37bf8f98565a5555ee4d4c6bf8889e0d7a723
Doyal-N <coin20@protonmail.com>
2021-05-05 19:10:51 +0500

    refact_games_controller_spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,105 +1,129 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
   let(:user) { create(:user) }
   let(:game) { create(:game_with_questions, user: user) }
 
-  describe 'GET #show' do
-    let(:current_question) { game.current_game_question }
+  context 'when authenticated user' do
 
-    before do
-      login(user)
-      get :show, params: { id: user.games.find(game.id) }
-    end
-
-    it 'assigns game_question' do
-      expect(assigns(:game_question)).to eq(current_question)
-    end
-
-    it 'render show' do
-      expect(response).to render_template :show
-    end
-  end
-
-  describe 'GET #show stranger game' do
-    let(:user2) { create(:user) }
-
-    it 'user do not show stranger game' do
-      login(:user2)
-      get :show, params: { id: game }
-
-      expect(response.status).to eq(302)
-      expect(response).to redirect_to(user_session_path)
-    end
-  end
-
-  describe 'PUT #take_money' do
-    it 'redirect_to user' do
-      login(user)
-      put :take_money, params: { id: game }
-
-      expect(response).to redirect_to user_path(user)
-      expect(flash[:warning]).to be
-    end
-  end
-
-  describe 'create double game' do
-    it "user can't to create second game" do
-      login(user)
-      game
-
-      expect { post :create }.to change(Game, :count).by(0)
-      expect(response).to redirect_to game_path(game)
-      expect(flash[:alert]).to be
-    end
-  end
-
-  describe 'anonim can not trigger all actions' do
-    it '#show' do
-      get :show, params: { id: game }
-
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-    end
-
-    it '#create' do
-      post :create
-
-      expect(response.status).not_to eq(200)
-      expect(response).to redirect_to(new_user_session_path)
-    end
-
-    it '#answer, #take_money, #help' do
-      %i[answer take_money help].each do |action|
-        put action, params: { id: game }
-
-        expect(response.status).not_to eq(200)
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-  end
-
-  describe 'PUT #answer' do
     before { login(user) }
 
-    context 'user answered correctly' do
-      it 'game continue and redirect to game' do
-        question = game.current_game_question
+    describe 'GET #show' do
+      before { get :show, params: { id: user.games.find(game.id) } }
 
-        put :answer, params: { id: game, letter: question.correct_answer_key }
+      it 'request assigns game_question' do
+        expect(assigns(:game_question)).to eq(game.current_game_question)
+      end
 
-        expect(response).to redirect_to game_path(game)
-        expect(game.finished?).to be_falsey
+      it 'render show' do
+        expect(response).to render_template :show
       end
     end
 
-    context 'user answered wrong' do
-      it 'game finish and redirect to user' do
-        put :answer, params: { id: game, letter: 'a' }
+    describe 'PUT #take_money' do
+      it 'redirects to user, changes status and finishes game' do
+        put :take_money, params: { id: game }
 
         expect(response).to redirect_to user_path(user)
+        expect(assigns(:game).status).to eq(:money)
         expect(assigns(:game).finished?).to be_truthy
+      end
+    end
+
+    describe 'PUT #answer' do
+      context 'user answered correctly' do
+        it 'game continue and redirect to game' do
+          question = game.current_game_question
+
+          put :answer, params: { id: game, letter: question.correct_answer_key }
+
+          expect(response).to redirect_to game_path(game)
+          expect(game.finished?).to be_falsey
+        end
+      end
+
+      context 'user answered wrong' do
+        it 'game finish and redirect to user' do
+          put :answer, params: { id: game, letter: 'a' }
+
+          expect(response).to redirect_to user_path(user)
+          expect(assigns(:game).finished?).to be_truthy
+        end
+      end
+    end
+
+    describe 'create double game' do
+      it "user can't to create second game" do
+        game
+        one_more_game = assigns(:game)
+
+        expect { post :create }.to change(Game, :count).by(0)
+        expect(response).to redirect_to game_path(game)
+        expect(one_more_game).to be_nil
+        expect(flash[:alert]).to be
+      end
+    end
+  end
+
+  context 'when other authenticated user' do
+    let(:user2) { create(:user) }
+
+    describe 'GET #show stranger game' do
+      it 'user do not show stranger game' do
+        login(:user2)
+        get :show, params: { id: game }
+
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+  end
+
+  context 'when anonimous user' do
+    describe 'anonim can not trigger all actions' do
+      describe 'GET #show' do
+        it 'can not show game and redirects to login' do
+          get :show, params: { id: game }
+
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+
+      describe 'POST #create' do
+        it 'can not create game and redirects to login' do
+          post :create
+
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+
+      describe 'PUT #answer' do
+        it 'can not give answer and redirects to login' do
+          put :answer, params: { id: game }
+
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+
+      describe 'PUT #take_money' do
+        it 'can not take money and redirects to login' do
+          put :take_money, params: { id: game }
+
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+
+      describe 'PUT #help' do
+        it 'can not get help and redirects to login' do
+          put :help, params: { id: game }
+
+          expect(response.status).not_to eq(200)
+          expect(response).to redirect_to(new_user_session_path)
+        end
       end
     end
   end
